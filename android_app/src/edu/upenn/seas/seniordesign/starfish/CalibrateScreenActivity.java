d9497203d827603c86bd98de830aa451cb81871c
martydd3 <dengmartin@hotmail.com>
2014-03-04 14:29:43 -0500

    Initial commit for calibration GUI - Martin

---
+++ android_app/src/edu/upenn/seas/seniordesign/starfish/CalibrateScreenActivity.java
@@ -0,0 +1,13 @@
+package edu.upenn.seas.seniordesign.starfish;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalibrateScreenActivity extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calibrate_screen);
+	}
+}
