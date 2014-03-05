f1809888516ddea148eb1ca6be596c339a1e48ee
martydd3 <dengmartin@hotmail.com>
2014-03-04 23:10:13 -0500

    basic calibration GUI framework

--- android_app/src/edu/upenn/seas/seniordesign/starfish/CalibrateScreenActivity.java
+++ android_app/src/edu/upenn/seas/seniordesign/starfish/CalibrateScreenActivity.java
@@ -2,6 +2,7 @@ package edu.upenn.seas.seniordesign.starfish;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CalibrateScreenActivity extends Activity{
 
@@ -9,5 +10,34 @@ public class CalibrateScreenActivity extends Activity{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calibrate_screen);
+	}
+	
+	/**
+	 * Calls all the testing methods from one location
+	 * @param view
+	 */
+	public void testAll(View view)
+	{
+		
+	}
+	
+	public void ledTest(View view)
+	{
+		
+	}
+	
+	public void pumpTest(View view)
+	{
+		
+	}
+	
+	public void heatTest(View view)
+	{
+		
+	}
+	
+	public void valvesTest(View view)
+	{
+		
 	}
 }
