73a87d746c46e5b27988b7b6b2fbb9d2758bb50e
Andrew Botelho <botelhoa92@gmail.com>
2014-04-18 04:17:36 -0400

    made LED calibration actually work

--- android_app2/src/edu/upenn/seas/senior_design/p2d2/CalibrateActivity.java
+++ android_app2/src/edu/upenn/seas/senior_design/p2d2/CalibrateActivity.java
@@ -8,10 +8,13 @@ import android.os.IBinder;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DialogFragment;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -22,18 +25,19 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Toast;
 
 public class CalibrateActivity extends Activity {
-	ActionArrayAdapter adapter;
-	// adapter is passed to ListView in order to initialize it
+	private LocalBroadcastManager manager;
 
-	ListView listview;
+	// adapter is passed to ListView in order to initialize it
+	ActionArrayAdapter adapter;
+
 	// listview holds the items that are touched by the user to run calibration
 	// tests
+	ListView listview;
 
-	ArrayList<ListItem> list;
 	// Holds ListItems that contains data for the tests in the ListView items
+	ArrayList<ListItem> list;
 
 	public static final String TAG = "edu.upenn.seas.seniordesign.starfish.CalibrateScreenActivity";
 
@@ -62,6 +66,41 @@ public class CalibrateActivity extends Activity {
 			DialogFragment btAlertFragment = new AlertDialogFragmentBT()
 					.newInstance();
 			btAlertFragment.show(getFragmentManager(), "no_BT");
+		}
+	};
+
+	private final BroadcastReceiver mBTDataReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String stringExtra = intent
+					.getStringExtra(getString(R.string.bt_data_type));
+			if (stringExtra == null) {
+				Log.e(BLUETOOTH_SERVICE,
+						"Null data type broadcast from BT service");
+				return;
+			}
+			if (stringExtra.equals(getString(R.string.bt_error))) {
+				processError(intent);
+			} else if (stringExtra.equals(getString(R.string.bt_fluid_state))) {
+				processFluidActuationState(intent);
+			} else if (stringExtra.equals(getString(R.string.bt_heating_state))) {
+				processHeatingState(intent);
+			} else if (stringExtra.equals(getString(R.string.bt_led_state))) {
+				processLEDState(intent);
+			} else if (stringExtra.equals(getString(R.string.bt_temp_data))) {
+				processTempData(intent);
+			}
+		}
+	};
+
+	private final BroadcastReceiver mBTStopReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			boolean shutdown = intent.getBooleanExtra(
+					getString(R.string.bt_disconnect_broadcast), false);
+			if (shutdown) {
+				unbindService(mConnection);
+			}
 		}
 	};
 
@@ -115,6 +154,18 @@ public class CalibrateActivity extends Activity {
 
 		Intent intent = new Intent(this, BTConnectionService.class);
 		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+		
+		// Register Broadcast receivers for BT Service
+		manager = LocalBroadcastManager.getInstance(getApplicationContext());
+		// Receiver for data
+		IntentFilter dataFilter = new IntentFilter(
+				BTConnectionService.ACTION_BT_RECIEVED);
+		manager.registerReceiver(mBTDataReceiver, dataFilter);
+		// Receiver for stopping service
+		IntentFilter stopFilter = new IntentFilter(
+				BTConnectionService.ACTION_BT_STOP);
+		manager.registerReceiver(mBTStopReceiver, stopFilter);
+
 	}// end of onCreate()
 
 	@Override
@@ -217,17 +268,32 @@ public class CalibrateActivity extends Activity {
 	}
 
 	public boolean ledTest() {
-		byte[] bytes = { (byte) 0xB8, (byte) 0xD3, (byte) 0x01, (byte) 0x3C,
+		byte[] turnOn = { (byte) 0xB8, (byte) 0xD3, (byte) 0x01, (byte) 0x3C,
 				(byte) 0xFF };
+		byte[] check = { (byte) 0xB8, (byte) 0xD3, (byte) 0x01, (byte) 0xFF,
+				(byte) 0x3C };
+		byte[] turnOff = { (byte) 0xB8, (byte) 0xD3, (byte) 0x01, (byte) 0x3C,
+				(byte) 0x00 };
 
-		if (list.get(0).testPassed()) {
-			bytes[4] = (byte) 0x00;
+		/*
+		 * if (list.get(0).testPassed()) { bytes[4] = (byte) 0x00; }
+		 */
+
+		mBTService.writeToBT(turnOn);
+		mBTService.writeToBT(check);
+
+		int count = 0;
+		while (!list.get(0).testPassed()) {
+			if (count > 200) {
+				break;
+			}
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {}
+			count++;
 		}
 
-		mBTService.writeToBT(bytes);
-
-		list.get(0).setTestResult(!list.get(0).testPassed());
-
+		mBTService.writeToBT(turnOff);
 		return list.get(0).testPassed();
 	}
 
@@ -260,4 +326,111 @@ public class CalibrateActivity extends Activity {
 
 		return list.get(2).testPassed();
 	}
+
+	/***************************************************************************
+	 * Methods for processing BT Data
+	 **************************************************************************/
+	private void processTempData(Intent intent) {
+		byte dataOne = intent.getByteExtra(getString(R.string.bt_data_1),
+				(byte) 0xFF);
+		if (dataOne == (byte) 0xFF) {
+			Log.e(BLUETOOTH_SERVICE, "Bad temp data reached processTempData()");
+			return;
+		}
+		byte dataTwo = intent.getByteExtra(getString(R.string.bt_data_2),
+				(byte) 0xFF);
+		int tempData = dataOne * 256 + dataTwo;
+		String tempDataString = Integer.toString(tempData);
+		Log.i(BLUETOOTH_SERVICE, "Temperature = " + tempDataString + " Celcius");
+	}
+
+	private void processHeatingState(Intent intent) {
+		byte dataOne = intent.getByteExtra(getString(R.string.bt_data_1),
+				(byte) 0xFA);
+		switch (dataOne) {
+		case (byte) 0x00:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Stopped");
+			break;
+		case (byte) 0x31:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heating to Temp 1");
+			break;
+		case (byte) 0x33:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heated to Temp 1");
+			break;
+		case (byte) 0x51:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heating to Temp 2");
+			break;
+		case (byte) 0x55:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heated to Temp 2");
+			break;
+		case (byte) 0x62:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heating to Temp 3");
+			break;
+		case (byte) 0x66:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heated to Temp 3");
+			break;
+		case (byte) 0xF7:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heating to Temp 4");
+			break;
+		case (byte) 0xFF:
+			Log.i(BLUETOOTH_SERVICE, "Heating state: Heated to Temp 4");
+			break;
+		default:
+			Log.e(BLUETOOTH_SERVICE,
+					"Bad fluid actuation state reached processFluidActuationState()");
+			break;
+		}
+	}
+
+	private void processLEDState(Intent intent) {
+		byte dataOne = intent.getByteExtra(getString(R.string.bt_data_1),
+				(byte) 0xFA);
+		switch (dataOne) {
+		case (byte) 0x00:
+			list.get(0).setTestResult(false);
+			Log.i(BLUETOOTH_SERVICE, "LEDS: OFF");
+			break;
+		case (byte) 0xFF:
+			list.get(0).setTestResult(true);
+			Log.i(BLUETOOTH_SERVICE, "LEDS: ON");
+			break;
+		default:
+			Log.e(BLUETOOTH_SERVICE, "Bad LED state reached processLEDState()");
+			break;
+		}
+	}
+
+	private void processFluidActuationState(Intent intent) {
+		byte dataOne = intent.getByteExtra(getString(R.string.bt_data_1),
+				(byte) 0xFA);
+		switch (dataOne) {
+		case (byte) 0x00:
+			Log.i(BLUETOOTH_SERVICE, "Fluids: not yet actuated");
+			break;
+		case (byte) 0x44:
+			Log.i(BLUETOOTH_SERVICE, "Fluids: currently actuating");
+			break;
+		case (byte) 0xFF:
+			Log.i(BLUETOOTH_SERVICE, "Fluids: already actuated");
+			break;
+		default:
+			Log.e(BLUETOOTH_SERVICE,
+					"Bad fluid actuation state reached processFluidActuationState()");
+			break;
+		}
+	}
+
+	private void processError(Intent intent) {
+		byte dataOne = intent.getByteExtra(getString(R.string.bt_data_1),
+				(byte) 0xFF);
+		switch (dataOne) {
+		case (byte) 0x11:
+			Log.e(BLUETOOTH_SERVICE, "Temperature out of range!");
+			break;
+		default:
+			Log.e(BLUETOOTH_SERVICE, "Bad error intent reached processError()");
+			break;
+		}
+	}
+
 }
